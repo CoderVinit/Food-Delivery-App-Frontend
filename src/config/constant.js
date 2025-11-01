189bcfca19f083d2784a6bc65dd30367126ed91d
VINIT KUMAR PATEL <101181057+CoderVinit@users.noreply.github.com>
2025-11-01 13:23:40 +0530

    Update BASE_URL constant to include commented-out production and local URLs

--- src/config/constant.js
+++ src/config/constant.js
@@ -1,2 +1,3 @@
-export const BASE_URL = "https://food-delivery-app-backend-1-rbt0.onrender.com"
+// export const BASE_URL = "https://food-delivery-app-backend-1-rbt0.onrender.com"
 // export const BASE_URL = "http://localhost:8080"
+export const BASE_URL = "http://65.1.112.98:8081"
